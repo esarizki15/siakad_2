fd7c19fbaaa79802db0077dae168f490dc0ba70c
adibingg <adihidayat.lpg@gmail.com>
2019-11-09 06:08:20 +0700

    FRONTEND perbaikan page data guru

--- resources/views/guru.blade.php
+++ resources/views/guru.blade.php
@@ -59,9 +59,10 @@
                             <td>{{$row->alamat}}</td>
                             <td>{{$row->telepon}}</td>
                             <td>{{$row->jp_id}}</td>
-                            <td>
-                                <a href="{{url('guru/edit/'.$row->guru_id)}}" class="btn btn-primary btn-xs"><i class="fa fa-edit"></i></a>
-                                <a href="{{url('guru/delete/'.$row->guru_id)}}" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a>
+                            <td width="100" class="text-center">
+                                 <a href="{{url('guru/edit/'.$row->guru_id)}}" class="btn btn-success btn-xs"><i class="fa fa-list"></i></a>
+                                 <a href="{{url('guru/edit/'.$row->guru_id)}}" class="btn btn-primary btn-xs"><i class="fa fa-edit"></i></a>
+                                 <a href="{{url('guru/delete/'.$row->guru_id)}}" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a>
                             </td>
                         </tr>
                      @endforeach
